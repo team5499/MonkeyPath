2c22e060652705d1def8e7f54eec0277022d7dbe
Andy Cate <and164v@gmail.com>
2019-01-21 21:54:11 -0800

    Fix path generation function

--- src/PathGen.js
+++ src/PathGen.js
@@ -1,47 +1,41 @@
-const $ = require('jquery');
-var java = require('java');
-var mvn = require('node-java-maven');
-var exports = module.exports = {};
+const java = require('java');
+const mvn = require('node-java-maven');
 
-mvn(function(err, mvnResults) {
-    if (err) {
-        return console.error('could not resolve maven dependencies', err);
+module.exports = {};
+
+mvn((err, mvnResults) => {
+  if (err) {
+    return console.error('could not resolve maven dependencies', err);
+  }
+  mvnResults.classpath.forEach((c) => {
+    console.log(`adding ${c} to classpath`);
+    java.classpath.push(c);
+  });
+
+  const Vector2 = java.import('org.team5499.monkeyLib.math.geometry.Vector2');
+  const Rotation2d = java.import('org.team5499.monkeyLib.math.geometry.Rotation2d');
+  const Pose2d = java.import('org.team5499.monkeyLib.math.geometry.Pose2d');
+  const PathGenerator = java.import('org.team5499.monkeyLib.path.PathGenerator');
+
+  function generatePath(points) {
+    console.log('running generatePath()');
+    const generator = new PathGenerator(1.1, 1.1, 1.1, 1.1);
+    const waypoints = [];
+    // var x = Double($(".x").val());
+    // var y = Double($(".y").val());
+    // var x = [0.0, 1.0, 3.0];
+    // var y = [0.0, 1.0, 3.0];
+    for (let i = 0; i < points.length; i += 1) {
+      waypoints.push(
+        Pose2d(Vector2(points[i][0], points[i][1]),
+          Rotation2d(points[i][0], points[i][1], false)),
+      );
     }
-    mvnResults.classpath.forEach(function(c) {
-        console.log('adding ' + c + ' to classpath');
-        java.classpath.push(c);
-    });
+    const waypointsJava = java.newArray('org.team5499.monkeyLib.math.geometry.Pose2d', waypoints);
+    const generated = generator.generatePathSync(false, waypointsJava, 1.1, 1.1, 1.1, 1.1);
+    return (generated);
+  }
 
-    var Position = java.import('org.team5499.monkeyLib.math.Position');
-    var JavaSystem = java.import('java.lang.System');
-    JavaSystem.out.printlnSync("hello from java");
-    var newPos = new Position();
-    newPos.updateSync(1.0, 1.0, 1.0);
-
-    function generatePath() {
-        console.log('ran generatePath()');
-        var Double = java.import('java.lang.Double');
-        var Vector2 = java.import('org.team5499.monkeyLib.math.geometry.Vector2');
-        var Rotation2d = java.import('org.team5499.monkeyLib.math.geometry.Rotation2d');
-        var Pose2d = java.import('org.team5499.monkeyLib.math.geometry.Pose2d');
-        var PathGen = java.import('org.team5499.monkeyLib.path.PathGenerator');
-        var generator = new PathGen(1.1, 1.1, 1.1, 1.1);
-        var waypoints = [];
-        var x = Double($(".x").val());
-        var y = Double($(".y").val()); 
-        // var x = [0.0, 1.0, 3.0];
-        // var y = [0.0, 1.0, 3.0];
-        for (i = 0; i <= x.length; x++) {
-            waypoints.push(
-                Pose2d(Vector2(x[i], y[i]),
-                Rotation2d(x[i], y[i], false)
-            ));
-        }
-        var generated = generator.generatePathSync(false, waypoints, Double(1.1), Double(1.1), Double(1.1), Double(1.1));
-        JavaSystem.out.printlnSync("hello from java");
-        return (generated);
-    }
-    console.log(newPos.toStringSync());
-
-    module.exports.generatePath = () => generatePath();
-    });
+  module.exports.generatePath = () => generatePath();
+  return console.log('Path generation initialized');
+});
